ec425e84e4ec9aabe97edf9011704d132ead953d
Lassi Rintala <lassi.rintala@gmail.com>
2018-04-19 15:37:24 +0300

    Added Scripts directory to Assets and BallLauncher script

---
+++ MobileGame/Assets/Scripts/BallLauncher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLauncher : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().AddForce(0.0f, 30.0f, 0.0f, ForceMode.VelocityChange);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
